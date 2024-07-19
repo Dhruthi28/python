e1fa6bbd5a1f0adba1aea7816e4f4807e9e3ae86
Dhruthi28 <137291294+Dhruthi28@users.noreply.github.com>
2024-07-19 12:17:09 +0530

    Add files via upload

---
+++ Pgm1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1a
+a = int(input("Enter 1 number "))
+b = int(input("Enter 2 number "))
+c = a+b
+print("Sum is", c)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
